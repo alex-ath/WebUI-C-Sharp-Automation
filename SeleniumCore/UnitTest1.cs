cd7f5c37d60cb2ac01ad59e52a5caf2fb99e6a50
alex-ath <alexath.contact@gmail.com>
2020-06-07 21:04:00 +0100

    Add project files.

---
+++ SeleniumCore/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
