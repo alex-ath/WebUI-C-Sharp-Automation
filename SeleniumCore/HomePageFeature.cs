5ee728b9f5f14027d0a5e793ed7991835da8d9d7
alex-ath <alexath.contact@gmail.com>
2020-06-08 00:00:58 +0100

    Intro to Selenium WebDriver with .NET Core

--- SeleniumCore/HomePageFeature.cs
+++ SeleniumCore/HomePageFeature.cs
@@ -1,16 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace SeleniumCore
 {
     [TestClass]
     public class HomePageFeature
     {
+        IWebDriver _driver;
         [TestMethod]
         public void ShouldBeAbleToLogin()
         {
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            //outPutDirectory helps to dynamically find the ChromeDriver's location
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _driver = new ChromeDriver(outPutDirectory);
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            var loginButtonLocator = By.ClassName("btn_action");
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(loginButtonLocator));
+
+            var userNameField = _driver.FindElement(By.Id("user-name"));
+            var passwordField = _driver.FindElement(By.Id("password"));
+            var loginButton = _driver.FindElement(loginButtonLocator);
+
+            userNameField.SendKeys("standard_user");
+            passwordField.SendKeys("secret_sauce");
+            loginButton.Click();
+
+            Assert.IsTrue(_driver.Url.Contains("inventory.html"));
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _driver.Quit();
         }
     }
 }
